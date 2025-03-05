9ca2ea870da17df3022b0ed3e8b6f541e1a38e2c
kenrique100 <ngwakenri2016@gmail.com>
2025-03-05 05:36:30 -0500

    model

---
+++ investment-service/src/main/java/com/akentech/kbf/investment/model/UpdateInvestmentRequest.java
@@ -0,0 +1,18 @@
+package com.akentech.kbf.investment.model;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateInvestmentRequest {
+    @NotNull(message = "New amount is required")
+    @DecimalMin(value = "1.0", message = "Amount must be greater than zero")
+    private BigDecimal newAmount;
+}
