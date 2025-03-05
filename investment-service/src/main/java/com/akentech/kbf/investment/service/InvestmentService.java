d9b3d86719245b89236ee93fdc2df6de625dcb4b
kenrique100 <ngwakenri2016@gmail.com>
2025-03-05 05:36:12 -0500

    service classes

---
+++ investment-service/src/main/java/com/akentech/kbf/investment/service/InvestmentService.java
@@ -0,0 +1,19 @@
+package com.akentech.kbf.investment.service;
+
+import com.akentech.kbf.investment.model.Investment;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+
+public interface InvestmentService {
+    Mono<Investment> createInvestment(BigDecimal initialAmount, String createdBy);
+    Mono<Investment> getInvestmentById(String id);
+    Flux<Investment> getAllInvestments();
+    Mono<Investment> updateInvestment(String id, BigDecimal newAmount);
+    Mono<Void> deleteInvestment(String id);
+    Mono<Investment> deductFromInvestment(String id, BigDecimal amount);
+    Mono<Investment> addToInvestment(String id, BigDecimal amount);
+    Mono<BigDecimal> getTotalInitialAmount();
+    Mono<BigDecimal> getTotalCurrentBalance();
+}
