6eb0469228cd9adc76325929bad957e03998ab2f
kenrique100 <ngwakenri2016@gmail.com>
2025-03-05 03:55:07 -0500

    main app

---
+++ investment-service/src/main/java/com/akentech/kbf/investment/InvestmentServiceApplication.java
@@ -0,0 +1,11 @@
+package com.akentech.kbf.investment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentsServiceApplication.class, args);
+    }
+}
