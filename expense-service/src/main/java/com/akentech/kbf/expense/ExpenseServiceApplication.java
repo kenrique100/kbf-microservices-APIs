cea2890c62ca01405671643a050017d296b90cc0
Edwin  Ngwa <ngwakenri2016@gmail.com>
2025-03-17 01:17:40 -0400

    commented

--- expense-service/src/main/java/com/akentech/kbf/expense/ExpenseServiceApplication.java
+++ expense-service/src/main/java/com/akentech/kbf/expense/ExpenseServiceApplication.java
@@ -3,7 +3,7 @@ package com.akentech.kbf.expense;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+/* Main App */
 @SpringBootApplication
 public class ExpenseServiceApplication {
     public static void main(String[] args) {
