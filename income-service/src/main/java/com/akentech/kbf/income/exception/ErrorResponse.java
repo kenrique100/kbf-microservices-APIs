459f1d60be2954c953d24e251bb4de7677a34c2c
Edwin  Ngwa <ngwakenri2016@gmail.com>
2025-03-26 15:24:42 -0400

    updated error message and timestamps

--- income-service/src/main/java/com/akentech/kbf/income/exception/ErrorResponse.java
+++ income-service/src/main/java/com/akentech/kbf/income/exception/ErrorResponse.java
@@ -1,13 +1,22 @@
 package com.akentech.kbf.income.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+import java.util.Map;
+
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ErrorResponse {
     private int status;
     private String message;
-}
+    private String errorCode;
+    private Instant timestamp;
+    private Map<String, String> details;
+
+    }
