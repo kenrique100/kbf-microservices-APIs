ece32179bd6461826acbedb9da8d555a71783f6b
Edwin  Ngwa <ngwakenri2016@gmail.com>
2025-03-27 16:09:32 -0400

    updates

--- transaction-service/src/main/java/com/akentech/kbf/transaction/repository/TransactionRepository.java
+++ transaction-service/src/main/java/com/akentech/kbf/transaction/repository/TransactionRepository.java
@@ -3,11 +3,7 @@ package com.akentech.kbf.transaction.repository;
 import com.akentech.kbf.transaction.model.Transaction;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
-
-import java.util.UUID;
 
 @Repository
 public interface TransactionRepository extends ReactiveCrudRepository<Transaction, Long> {
-    Flux<Transaction> findByType(String type);
 }
